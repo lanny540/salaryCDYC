8704b8277903a737de614193ef30f6797a2d3749
lanny540 <20512776@qq.com>
2020-04-03 10:54:51 +0800

    个税计算

--- app/Http/Controllers/HelpController.php
+++ app/Http/Controllers/HelpController.php
@@ -46,10 +46,11 @@ class HelpController extends Controller
      */
     public function taxCalculate(Request $request)
     {
-//        TODO:个税计算方法
-        $data = $request->get('money', 0);
+        $salary = $request->get('money', 0);
 
-        return response()->json($data);
+        $tax = $this->getPersonalIncomeTax($salary);
+
+        return response()->json($tax);
     }
 
     /**
@@ -75,4 +76,46 @@ class HelpController extends Controller
 
         return redirect()->route('report')->withSuccess('感谢您的宝贵意见!');
     }
+
+    /**
+     * 个税计算公式.
+     *
+     * @param float $salary 含税收入金额
+     * @param int $deduction 保险等应当扣除的金额 默认值为0
+     * @param int $threshold 起征金额 默认值为5000
+     * @return float | false 返回值为应缴税金额 参数错误时返回false
+     */
+    private function getPersonalIncomeTax($salary, $deduction=0, $threshold=5000){
+
+        if(!is_numeric($salary) || !is_numeric($deduction) || !is_numeric($threshold)){
+            return false;
+        }
+
+        if($salary <= $threshold){
+            return 0;
+        }
+
+        $levels = [3000, 12000, 25000, 35000, 55000, 80000, PHP_INT_MAX];
+        $rates = [0.03, 0.1, 0.2, 0.25, 0.3, 0.35, 0.45];
+
+        $taxableIncome = $salary - $threshold - $deduction;
+
+        $tax = 0;
+
+        foreach($levels as $k => $level){
+            $previousLevel = $levels[$k - 1] ?? 0;
+            if($taxableIncome <= $level){
+                $tax += ($taxableIncome - $previousLevel) * $rates[$k];
+
+                break;
+            }
+
+            $tax += ($level-$previousLevel) * $rates[$k];
+        }
+
+        $tax = round($tax, 2);
+
+        return $tax;
+    }
+
 }
