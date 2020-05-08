14f373dfa64231c6816d5802560af887ebb3d0b1
lanny540 <20512776@qq.com>
2020-05-08 11:28:56 +0800

    流程表增加记录数和合计金额

--- database/migrations/2020_03_24_114436_create_workflows_tables.php
+++ database/migrations/2020_03_24_114436_create_workflows_tables.php
@@ -10,10 +10,13 @@ class CreateWorkflowsTables extends Migration
     {
         Schema::create('workflows', function (Blueprint $table) {
             $table->bigIncrements('id');
+            $table->integer('period_id')->comment('会计期ID');
             $table->string('name')->comment('流程名称');
             $table->integer('uploader')->index()->comment('上传用户ID');
             $table->string('upload_file')->nullable()->comment('上传文件地址');
             $table->boolean('isconfirm')->default(false)->comment('上传数据是否被财务确认');
+            $table->integer('record')->default(0)->comment('数据条数');
+            $table->decimal('money', 14, 2)->default(0.00)->comment('合计金额');
             $table->softDeletes();
             $table->timestamps();
         });
